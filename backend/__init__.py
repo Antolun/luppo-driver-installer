9886bd2134ce98f90544fd285f99736d163a5cf1
Solzic0 <solzic0ta@gmail.com>
2026-08-11 13:59:02 +0300

    New Update

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Backend package initialization.
-"""
-from backend.hardware_detector import HardwareDetector
-from backend.pisi_backend import PisiBackend
-
-__all__ = ["HardwareDetector", "PisiBackend"]
--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Models package initialization.
-"""
-from models.gpu_device import GPUDevice, DriverOption, DriverStatus
-
-__all__ = ["GPUDevice", "DriverOption", "DriverStatus"]
