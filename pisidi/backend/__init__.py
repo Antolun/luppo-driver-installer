eaf5b247b43fcfe4d0117ea78095560169d7b9e4
Solzic0 <solzic0ta@gmail.com>
2026-08-15 19:17:28 +0300

    New Update

--- pisidi/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Backend package initialization.
-"""
-from pisidi.backend.hardware_detector import HardwareDetector
-from pisidi.backend.pisi_backend import PisiBackend
-
-__all__ = ["HardwareDetector", "PisiBackend"]
--- pisidi/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Models package initialization.
-"""
-from pisidi.models.gpu_device import GPUDevice, DriverOption, DriverStatus
-
-__all__ = ["GPUDevice", "DriverOption", "DriverStatus"]
